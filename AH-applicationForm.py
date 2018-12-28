4fbf7d595bbf4076ee5a583470f63333f6c980b2
Katya <katjaTomp@users.noreply.github.com>
2018-12-28 11:37:31 +0100

    Update AH-applicationForm.py

--- AH-applicationForm.py
+++ AH-applicationForm.py
@@ -331,18 +331,7 @@ def predict(model, img):
 
 model = loadModel()
 
-imags = ['/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/other:test/1617538-3.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/other:test/1685924-1.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/other:test/2617020-1.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/1589207-1.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/1593369-1.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/1593672-2.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/1594397-2.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/2590215-1.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/1610823-2.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/1936424-1.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/1689793-1.png',
-         '/Users/ktompoidi/Documents/tensorFlowDL/data/forms_2cat_1/test/id/1610823-5.png']
+imags = []
 
 for i in imags:
     print(i)
